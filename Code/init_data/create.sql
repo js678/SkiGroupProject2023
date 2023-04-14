637ca27348ef231b13e6f2bd9889e581a99a3b5a
Conrad Barron <conrad.barron@colorado.edu>
2023-04-13 18:00:54 -0600

    Added the database

--- Code/init_data/create.sql
+++ Code/init_data/create.sql
@@ -1,4 +1,18 @@
 CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) PRIMARY KEY,
+    email VARCHAR(200) NOT NULL,
     password CHAR(60) NOT NULL
+);
+
+CREATE TABLE user_to_trips(
+    user_id SMALLINT,
+    trip_id SMALLINT
+);
+
+CREATE TABLE past_trips(
+    trip_id SERIAL PRIMARY KEY,
+    link VARCHAR(200) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    duration INTEGER NOT NULL
 );
