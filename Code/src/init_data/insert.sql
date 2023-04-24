a8e35f56979f5a0b772dc4228e2fa0616727724a
Jack Savio <jacksavio678@gmail.com>
2023-04-24 16:35:47 -0600

    final commits for search database

--- Code/src/init_data/insert.sql
+++ Code/src/init_data/insert.sql
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 INSERT INTO users(username, email, password) VALUES ('bob', 'billy@gmail.com', 'asdasd');
 
 
@@ -22,8 +21,36 @@ INSERT INTO products(product_type, price, brand, name, image) VALUES ('Poles','1
 INSERT INTO products(product_type, price, brand, name, image) VALUES ('Poles','120.71','G3','VIA Carbon Telescopic', 'https://content.backcountry.com/images/items/900/GGG/GGGW18Q/BLA.jpg');
 INSERT INTO products(product_type, price, brand, name, image) VALUES ('Poles','189.96','Scott','RC Pro', 'https://content.backcountry.com/images/items/900/SCO/SCOL15Z/YEL.jpg');
 INSERT INTO products(product_type, price, brand, name, image) VALUES ('Poles','63.57','Camp USA','Piz Aluminum', 'https://content.backcountry.com/images/items/900/CMP/CMPJ18U/ONECOL.jpg');
-=======
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Goggles','289.00','WHITESPACE','RVX Mag', 'https://content.backcountry.com/images/items/900/WIE/WIE000Y/MAWMIAM.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Goggles','171.74','Oakley','Mikaela Shiffrin Signature', 'https://content.backcountry.com/images/items/900/OAK/OAKK8IT/MISHSIABBLPRJA.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Goggles','132.96','Dragon','NFX2', 'https://content.backcountry.com/images/items/900/DRA/DRA005Q/BLMILLDASMLBLLAMBOLE.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Goggles','71.97','Electric','Kleveland', 'https://content.backcountry.com/images/items/900/ELC/ELCZ66K/AUREMASPBL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Goggles','171.50','Smith','I/O MAG XL Low Bridge', 'https://content.backcountry.com/images/items/900/SMI/SMIZ9DQ/BLCHSUBL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Ski Boots','429.96','Atomic','Hawk Ultra 110 S', 'https://content.backcountry.com/images/items/900/ATO/ATOK1DZ/DARBLU.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Ski Boots','419.96','K2','Revolver Team', 'https://content.backcountry.com/images/items/900/K2S/K2SO0LS/BEISWI.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Ski Boots','519.96','Solomon','S/Pro 120 GW', 'https://content.backcountry.com/images/items/900/SAL/SALZ9WF/BLA.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Ski Boots','559.91','Dalbello Sports','Krypton 130', 'https://content.backcountry.com/images/items/900/DBL/DBLN090/RACGREBLU.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Ski Boots','249.92','Rossignol','Pure Pro 90', 'https://content.backcountry.com/images/items/900/ROS/ROSZ74K/WHIGRE.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','321.96','Bataleon','Party Wave', 'https://content.backcountry.com/images/items/900/BAT/BATF03W/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','422.27','Capita','Kazu Kokubo', 'https://content.backcountry.com/images/items/900/CPT/CPTO08K/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','384.99','Jones Snowboards','Mountain Twin', 'https://content.backcountry.com/images/items/900/JSB/JSBH074/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','472.00','Never Summer','Protosynthesis', 'https://content.backcountry.com/images/items/900/NVS/NVSR16Y/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','356.21','Slash','Spectrum', 'https://content.backcountry.com/images/items/900/SLH/SLHE00U/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','584.99','Cardiff Snowcraft','Crane Enduro', 'https://content.backcountry.com/images/items/900/CDS/CDS001K/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','420.00','Yes.','Warna Uninc', 'https://content.backcountry.com/images/items/900/YES/YESG03S/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','515.98','Arbor','Satori Camber', 'https://content.backcountry.com/images/items/900/ARB/ARBY16U/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','460.00','Whitespace','Freestyle Shaun White', 'https://content.backcountry.com/images/items/900/WIE/WIE0005/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboards','213.99','Gnu','Money Snowboard', 'https://content.backcountry.com/images/items/900/GNU/GNUL141/ONECOL.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboard Boots','212.29','ThirtyTwo','Shifty Boa', 'https://content.backcountry.com/images/items/900/TTW/TTWT15B/GRE.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboard Boots','464.62','Burton','Ion Boa', 'https://content.backcountry.com/images/items/900/BUR/BURZ970/SUNDRITOM.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboard Boots','389.99','DC','Control Step-On BOA', 'https://content.backcountry.com/images/items/900/DCS/DCSZ9BW/BLA.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboard Boots','122.99','Northwave Snow','Domain SLS', 'https://content.backcountry.com/images/items/900/NTH/NTHN14T/DARBRO.jpg');
+INSERT INTO products(product_type, price, brand, name, image) VALUES ('Snowboard Boots','599.99','Deeluxe','AREth RIN', 'https://content.backcountry.com/images/items/900/DEE/DEEI02U/DES.jpg');
+
+
+
+
+
 INSERT INTO users(username, email, password) VALUES ('bob', 'billy@gmail.com', '$2b$10$ZtZmowrkVcvTqPPx4wcV6eQAL.mOIUWItHr7Ntmlan7nBw1VCEZX.');
 INSERT INTO user_to_trips(user_id, trip_id) VALUES (1,1);
 INSERT INTO past_trips(link, location, duration) VALUES ('https://cuboulder-csci3308.pages.dev/docs/intro/', 'colorado', 3);
->>>>>>> Stashed changes
